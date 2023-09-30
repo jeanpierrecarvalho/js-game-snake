122ad0effab020cf505acc0995a06b45f9f43a06
jeanpierrecarvalho <90358872+jeanpierrecarvalho@users.noreply.github.com>
2023-09-30 16:39:23 +0100

    feat: controller with keyboard listener to control the snake and restart the game

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const controller = (event: KeyboardEvent) => {
+  if (event.key === 'r') console.log('Restarting the game...');
+  else if (event.key === 'ArrowRight') console.log('Arrow Right');
+  else if (event.key === 'ArrowLeft') console.log('Arrow Left');
+  else if (event.key === 'ArrowUp') console.log('Arrow Up');
+  else if (event.key === 'ArrowDown') console.log('Arrow Down');
+};
+
+document.addEventListener('keydown', controller);
